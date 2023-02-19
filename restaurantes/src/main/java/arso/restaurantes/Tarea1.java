02e83701441b10735d29488f605b860bf10b00f1
silvat0 <silvia.perezr@um.es>
2023-02-20 00:14:43 +0100

    Tarea 2 actualizada.

--- restaurantes/src/main/java/arso/restaurantes/Tarea1.java
+++ restaurantes/src/main/java/arso/restaurantes/Tarea1.java
@@ -1,5 +1,9 @@
 package arso.restaurantes;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -32,13 +36,11 @@ public class Tarea1 {
 		// 3. Analizar el documento
 		// documento es la raiz
 		Document documento = analizador.parse(
-				"http://api.geonames.org/findNearbyWikipedia?postalcode=" + codigoPostal + "&country=ES&username=arso");
+				"http://api.geonames.org/findNearbyWikipedia?postalcode=" + codigoPostal + "&country=ES&lang=es&username=arso");
 
 		// Obtenemos todos los elementos del fichero .xml con el tag name "entry"
 		NodeList lugares = documento.getElementsByTagName("entry");
 
-		// Println para mostrar el número de elementos que contiene el fichero .xml con
-		// el tag name "entry"
 		// System.out.println(lugares.getLength());
 
 		// Bucle para iterar los elementos con el tag name "entry"
@@ -57,18 +59,30 @@ public class Tarea1 {
 			String nombreLugar = nombre.getTextContent();
 			String nombreWiki = wikipedia.getTextContent();
 
-			System.out.println(nombreLugar);
+			//System.out.println(nombreLugar);
 
 			String[] urlWikipedia = nombreWiki.split("/");
+			//System.out.println(urlWikipedia);
 			String nombreToDBPedia = urlWikipedia[urlWikipedia.length - 1];
+			System.out.println(nombreToDBPedia);
 			// Aqui termino la obtención del nombre de la URL de wikipedia
 
-			// Object obj = parser.parse(new FileReader("https://es.dbpedia.org/data/" +
-			// nombreToDBPedia) + ".json");
+			//Usar reader de json --> Json.createReader(url).readObject
 			String url = "https://es.dbpedia.org/data/" + nombreToDBPedia + ".json";
 			BufferedReader lector = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+			JsonReader jsonReader = Json.createReader(lector);
+			JsonObject obj = jsonReader.readObject();
+			
+			
+			JsonObject resume = obj.getJsonObject("http://es.dbpedia.org/resource/" + nombreToDBPedia);
+			
+			JsonArray aux = resume.getJsonArray("http://dbpedia.org/ontology/abstract");
+			
+			for(JsonObject d : aux.getValuesAs(JsonObject.class)) {
+				System.out.println(d.getString("value"));
+			}
 
-			String respuesta = "";
+			/*String respuesta = "";
 			String linea;
 			while ((linea = lector.readLine()) != null) {
 				respuesta += linea;
@@ -80,19 +94,19 @@ public class Tarea1 {
 			Object objeto = parser.parse(respuesta);
 			JSONObject objetoJson = (JSONObject) objeto;
 
-//			JSONArray array = (JSONArray) objetoJson.get("http://dbpedia.org/ontology/abstract");
-//
-//			if (array != null) {
-//
-//				for (int j = 0; j < array.size(); j++) {
-//					JSONObject prueba = (JSONObject) array.get(j);
-//
-//					String nombrePlato = (String) prueba.get("type");
-//					System.out.println("Type : " + nombrePlato);
-//				}
-//			}
-			
-			System.out.println(objetoJson.toJSONString());
+			/*JSONArray array = (JSONArray) objetoJson.get("http://dbpedia.org/ontology/abstract");
+
+			if (array != null) {
+
+				for (int j = 0; j < array.size(); j++) {
+					JSONObject prueba = (JSONObject) array.get(j);
+
+					String nombrePlato = (String) prueba.get("type");
+					System.out.println("Type : " + nombrePlato);
+				}
+			}
+			*/
+			//System.out.println(objetoJson.toJSONString());*/
 
 		}
 	}
