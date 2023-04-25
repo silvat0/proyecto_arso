562074aa7fbe7a527589eade9c12b58ec85060f2
Raúl Hernández Martínez <91872253+raul-umu@users.noreply.github.com>
2023-04-25 21:04:06 +0200

    Puebas con PostMan

---
+++ restaurantes-rest/src/main/java/restaurantes/test/RepositorioTest.java
@@ -0,0 +1,39 @@
+package restaurantes.test;
+
+import arso.restaurantes.modelo.Plato;
+import arso.restaurantes.modelo.Restaurante;
+import arso.repositorio.memoria.RepositorioMemoria;
+import arso.repositorio.memoria.RepositorioException;
+
+
+public class RepositorioTest extends RepositorioMemoria<Restaurante> {
+
+	public RepositorioTest() {
+		
+		 // Restaurante 1
+        
+   		Restaurante restaurante1 = new Restaurante();
+   		restaurante1.setNombre("Restaurante Emboka");
+   		restaurante1.setCoordenadas("JUAN DE BORBON, 29, 30007 Murcia");
+
+		// Plato 1
+
+		Plato plato1 = new Plato();		
+		plato1.setNombre("VIOLETA POCHE");
+		plato1.setDescripcion("Huevo trufado con patata francesa violeta y seta de cardo emulsionada con aceite de trufa\r\n"
+				+ "\r\n"
+				+ "puede ser sin gluten\r\n"
+				+ "\r\n"
+				+ "contine huevo");
+		
+		restaurante1.addPlato(plato1);
+		
+		try {
+			add(restaurante1);
+		} catch (RepositorioException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
