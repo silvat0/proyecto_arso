e34ac93f1c777dce1a00fb58f2e740deb1111d79
silvat0 <silvia.perezr@um.es>
2023-04-30 17:25:30 +0200

    Siguiendo con graphql para solucionar el error

--- opiniones-graphql/src/main/java/arso/opiniones/graphql/Query.java
+++ opiniones-graphql/src/main/java/arso/opiniones/graphql/Query.java
@@ -1,11 +1,9 @@
 package arso.opiniones.graphql;
 
-import java.util.List;
 
 import com.coxautodev.graphql.tools.GraphQLRootResolver;
 
 import arso.opiniones.modelo.Opinion;
-import arso.opiniones.servicios.OpinionResumen;
 import arso.opiniones.servicios.FactoriaServicios;
 import arso.opiniones.servicios.IServicioOpiniones;
 import arso.repositorio.memoria.EntidadNoEncontrada;
@@ -21,10 +19,5 @@ public class Query implements GraphQLRootResolver {
 		
 	}
 	
-	public List<OpinionResumen> getListadoOpiniones() throws RepositorioException {
-		
-		return servicio.getListadoOpiniones();
-			
-	}
-
+	
 }
